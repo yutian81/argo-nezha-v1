c1030ad625d125ea27b5edef81a6736f21eedd18
sfun <jiegto@outlook.com>
2024-12-02 09:53:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
-cloudflared tunnel --no-autoupdate run --token $CF_TOKEN & \
-nginx & \
-exec /dashboard/app
+
+# 启动 cloudflared 隧道
+cloudflared tunnel --no-autoupdate run --token $CF_TOKEN &
+cf_pid=$!
+
+# 启动 Nginx
+nginx -g "daemon off;" &
+nginx_pid=$!
+
+# 启动 /dashboard/app
+exec /dashboard/app &
+app_pid=$!
+
+# 等待所有后台进程完成
+wait $cf_pid $nginx_pid $app_pid
