c09170578e617293699d3afcd91642b53f746fe7
sfun <jiegto@outlook.com>
2024-12-02 15:12:16 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,17 +7,11 @@ nginx -t
 # 启动 Nginx
 echo "Starting nginx..."
 nginx -g "daemon off;" &
-nginx_pid=$!
 
 # 启动 cloudflared 隧道
 echo "Starting cloudflared..."
 cloudflared tunnel --no-autoupdate run --token $CF_TOKEN &
-cf_pid=$!
 
 # 启动 /dashboard/app
 echo "Starting dashboard app..."
 exec /dashboard/app &
-app_pid=$!
-
-# 等待所有后台进程完成
-wait $cf_pid $nginx_pid $app_pid
