4bac19e51bb069ebbb0018abb3364bd517ffcb7d
sfun <jiegto@outlook.com>
2024-12-02 11:22:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,5 @@ cloudflared tunnel --no-autoupdate run --token $CF_TOKEN &
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
 
 # 启动 /dashboard/app
+printf "nameserver 127.0.0.11\nnameserver 8.8.4.4\nnameserver 223.5.5.5\n" > /etc/resolv.conf
 exec /dashboard/app
