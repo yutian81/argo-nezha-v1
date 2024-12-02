c132407ff9d3d5f8d96bab4997cc1e9654eb772c
sfun <jiegto@outlook.com>
2024-12-02 10:29:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,17 @@
 ls -l /usr/local/nginx &
 
 # 启动 cloudflared 隧道
-cloudflared tunnel --no-autoupdate run --token $CF_TOKEN &
-cf_pid=$!
+#cloudflared tunnel --no-autoupdate run --token $CF_TOKEN &
+#cf_pid=$!
 
 # 启动 Nginx
 /usr/sbin/nginx -g "daemon off;" &
 nginx_pid=$!
 
 # 启动 /dashboard/app
-exec /dashboard/app &
-app_pid=$!
+#exec /dashboard/app &
+#app_pid=$!
 
 # 等待所有后台进程完成
-wait $cf_pid $nginx_pid $app_pid
+#wait $cf_pid $nginx_pid $app_pid
+wait $nginx_pid
